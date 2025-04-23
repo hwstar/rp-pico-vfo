9562896bfbe9b5fc6e8ed2b740a371d1328e80c5
Steve Rodgers <steve@rodgers619.com>
2025-04-23 15:07:22 -0700

    Fix product detector/balanced modulator injection frequency

--- include/config_default.h
+++ include/config_default.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define CONFIG_DEFAULT_IF_ZERO_HZ_FREQ 12287407 /* 12.287407 MHz */
+#define CONFIG_DEFAULT_IF_ZERO_HZ_FREQ 12288007 /* 12.287407 MHz */
 #define CONFIG_DEFAULT_REF_CLK_FREQ 26000000 /* 26 MHz */
 #define CONFIG_DEFAULT_REF_CLK_CAL -1000 /* Reference oscillator calibration */
 #define CONFIG_DEFAULT_STEP_SIZE 1000 /* Tuning knob step size  in hz */
